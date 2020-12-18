1896126fd60bd1001e8c45390dd61b95aecd43f0
Eren <68470782+eren-darici@users.noreply.github.com>
2020-12-18 20:12:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import pandas as pd
 class App:
     def __init__(self):
         """Initialization of Class with Lists"""
-        self._ingridients = list()
+        self._ingredients = list()
         self._numberofPeople = int()
-        self._ingridientAmount = list()
+        self._ingredientAmount = list()
         tempList = list()
-        self._finalIngrid = list()
+        self._finalIngred = list()
 
         self._recipes = list()
         self._howTo = list()
@@ -17,9 +17,9 @@ class App:
         recipeDF = pd.read_excel(file)
 
         for i in recipeDF.index:
-            self._recipes.append([recipeDF["Recipe"][i], recipeDF["Ingridients"][i].split(",")])
+            self._recipes.append([recipeDF["Recipe"][i], recipeDF["Ingredients"][i].split(",")])
             self._howTo.append(recipeDF["howTo"][i])
-            tempList.append(recipeDF["IngridientAmounts"][i].split(","))
+            tempList.append(recipeDF["IngredientAmounts"][i].split(","))
 
         self.df = recipeDF
 
@@ -27,23 +27,23 @@ class App:
             placeholder = []
             for item in sublist:
                 placeholder.append(int(item))
-            self._ingridientAmount.append(placeholder)
+            self._ingredientAmount.append(placeholder)
 
         del tempList
 
-    def addIngridient(self, ingridientName, ingridientAmount):
-        """Method to add Ingridients that we have"""
-        temp = [ingridientName, ingridientAmount]
-        self._ingridients.append(temp)
+    def addIngredient(self, ingredientName, ingredientAmount):
+        """Method to add Ingredients that we have"""
+        temp = [ingredientName, ingredientAmount]
+        self._ingredients.append(temp)
 
-    def showIngridients(self):
-        """Method to show Ingridients that we have"""
+    def showIngredients(self):
+        """Method to show Ingredients that we have"""
         temp1 = list()
         temp2 = list()
-        for i in self._ingridients:
+        for i in self._ingredients:
             temp1.append(i[0])
             temp2.append(i[1])
-        out = pd.DataFrame({"Ingridients": temp1, "Amount": temp2})
+        out = pd.DataFrame({"Ingredients": temp1, "Amount": temp2})
         return out
 
     def recipes(self):
@@ -51,15 +51,15 @@ class App:
         return self._recipes
 
     def recipeCheck(self):
-        """The recipes that we can make with ingridients"""
+        """The recipes that we can make with ingredients"""
         canDo = list()
-        for i in self._ingridients:
-            self._finalIngrid.append(i[0])
+        for i in self._ingredients:
+            self._finalIngred.append(i[0])
 
         for i in range(len(self._recipes)):
 
-            check = all(item in self._finalIngrid for item in self._recipes[i][1])
-            # print(self._finalIngrid)
+            check = all(item in self._finalIngred for item in self._recipes[i][1])
+            # print(self._finalIngred)
             if check is True:
                 canDo.append(self._recipes[i][0])
             else:
