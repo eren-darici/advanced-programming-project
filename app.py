f226552112f3de3ad2db43b381f7e31f5dfbfe29
Eren <68470782+eren-darici@users.noreply.github.com>
2021-01-02 10:04:04 +0300

    Final Commit

--- app.py
+++ app.py
@@ -2,73 +2,51 @@ import pandas as pd
 
 
 class App:
+
     def __init__(self):
-        """Initialization of Class with Lists"""
-        self._ingredients = list()
-        self._numberofPeople = int()
-        self._ingredientAmount = list()
-        tempList = list()
-        self._finalIngred = list()
+        self._foodDict = dict()
 
         self._recipes = list()
-        self._howTo = list()
 
-        file = "recipes.xlsx"
-        recipeDF = pd.read_excel(file)
+        self._df = pd.read_excel("recipes.xlsx")
 
-        for i in recipeDF.index:
-            self._recipes.append([recipeDF["Recipe"][i], recipeDF["Ingredients"][i].split(",")])
-            self._howTo.append(recipeDF["howTo"][i])
-            tempList.append(recipeDF["IngredientAmounts"][i].split(","))
-
-        self.df = recipeDF
-
-        for sublist in tempList:
-            placeholder = []
-            for item in sublist:
-                placeholder.append(int(item))
-            self._ingredientAmount.append(placeholder)
-
-        del tempList
+        for i in self._df.index:
+            recipeDict = {}
+            foodList = []
+            amountList = []
+            foodList.append(self._df["Ingredients"][i].split(","))
+            amountList.append(self._df["IngredientAmounts"][i].split(","))
+            for i in foodList[0]:
+                recipeDict[i] = int(amountList[0][foodList[0].index(i)])
+            self._recipes.append(recipeDict)
 
     def addIngredient(self, ingredientName, ingredientAmount):
-        """Method to add Ingredients that we have"""
-        temp = [ingredientName, ingredientAmount]
-        self._ingredients.append(temp)
+        if ingredientAmount > 0:
+            self._foodDict[ingredientName] = ingredientAmount
 
     def showIngredients(self):
-        """Method to show Ingredients that we have"""
-        temp1 = list()
-        temp2 = list()
-        for i in self._ingredients:
-            temp1.append(i[0])
-            temp2.append(i[1])
-        out = pd.DataFrame({"Ingredients": temp1, "Amount": temp2})
-        return out
+        IngredData = {"Ingredient": self._foodDict.keys(), "Amount": self._foodDict.values()}
+        return pd.DataFrame(data=IngredData)
 
     def recipes(self):
-        """Recipe List"""
-        return self._recipes
+        df = self._df.copy()
+        df.pop("IngredientAmounts")
+        return df
 
-    def recipeCheck(self):
-        """The recipes that we can make with ingredients"""
-        canDo = list()
-        for i in self._ingredients:
-            self._finalIngred.append(i[0])
-
-        for i in range(len(self._recipes)):
-
-            check = all(item in self._finalIngred for item in self._recipes[i][1])
-            # print(self._finalIngred)
-            if check is True:
-                canDo.append(self._recipes[i][0])
-            else:
-                pass
-
-        return canDo
-
-    def recipeDFret(self):
-        pass
+    def recipeCheck(self, numberofPeople=1):
+        foodNames = [x for x in self._df["Recipe"]]
+        checkList = []
+        for i in self._recipes:
+            lenFood = len(i)
+            tempList = []
+            for j in i.keys():
+                if j in self._foodDict.keys():
+                    if self._foodDict[j] >= i[j] * numberofPeople:
+                        tempList.append(foodNames[self._recipes.index(i)])
+                if len(tempList) == lenFood:
+                    for x in list(set(tempList)):
+                        checkList.append(x)
+        return checkList
 
 
 app = App()
--- main.py
+++ main.py
@@ -3,28 +3,47 @@ from app import App
 app = App()
 
 while True:
-    print("1-) add ingredient\n"
-          "2-) show ingredients\n"
-          "3-) recipe list\n"
-          "4-) recipe check\n"
-          "q-) exit")
+    print("1-) Add ingredient\n"
+          "2-) Show ingredients\n"
+          "3-) Recipe list\n"
+          "4-) Recipe check\n"
+          "q-) Exit")
+
+    print("********************")
 
     userInput = input("please select an event: ")
 
+    print("********************")
+
     if userInput == "q":
-        print("exit")
+        print("Exit")
+        print("********************")
         break
 
     elif userInput == "1":
-        ingredient = input("plz enter ingredient: ").upper()
-        amount = input("plz enter amount of ingredient: ")
-        app.addIngredient(ingredient, amount)
+        print("**Add Ingredient**")
+        ingredient = input("Please enter ingredient: ").upper()
+        amount = input("Please enter the amount of ingredient: ")
+        app.addIngredient(ingredient, int(amount))
+        print("********************")
+
 
     elif userInput == "2":
+        print("*Show Ingredients*")
         print(app.showIngredients())
+        print("********************")
+
     elif userInput == "3":
-         print(app.recipes())
+        print("**Show All Recipes**")
+        print(app.recipes())
+        print("********************")
+
     elif userInput == "4":
-        print(app.recipeCheck())
+        print("***Check Recipess***")
+        numberOfPeople = input("Please enter the number of people: ")
+        print(app.recipeCheck(int(numberOfPeople)))
+        print("********************")
+
     else:
-        print("invalid event")
+        print("Invalid event.")
+        print("********************")
